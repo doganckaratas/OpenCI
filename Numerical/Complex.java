64b47c289e64b73b54acb4bed69cee52ba423ee4
Doğan Can KARATAŞ <dogan.karatas@yandex.com>
2017-05-12 02:41:14 +0300

    Initial Release

---
+++ Numerical/Complex.java
@@ -0,0 +1,93 @@
+/**
+ * Complex.java
+ *
+ * @author dogan can karatas
+ * @version 1.0
+ */
+
+package Numerical;
+
+public class Complex {
+    private double real;
+    private double imag;
+	private String unit = "degree"; // "degree", "radian"
+
+    public Complex() {
+        this.real = 0.0D;
+        this.imag = 0.0D;
+    }
+
+    public Complex(double i, double j) {
+        this.real = i;
+        this.imag = j;
+    }
+
+    public void set(Complex c) {
+        this.real = c.getReal();
+        this.imag = c.getImag();
+    }
+	
+	public void set(double i, double j) {
+		this.real = i;
+		this.imag = j;
+	}
+
+    public Complex get() {
+        return new Complex(this.real, this.imag);
+    }
+
+    public void setReal(double i) {
+        this.real = i;
+    }
+
+    public void setImag(double j) {
+        this.imag = j;
+    }
+
+    public double getReal() {
+        return this.real;
+    }
+
+    public double getImag() {
+        return this.imag;
+    }
+
+    public boolean compareTo(Complex c) {
+        if (this.real == c.getReal() && this.imag == c.getImag()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean compareTo(double i, double j) {
+        if (this.real == i && this.imag == j) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+	public Polar toPolar() {
+		try{
+			switch(this.unit) {
+				case "degree":
+					return new Polar( Math.sqrt(Math.pow(this.real,2) + Math.pow(this.imag,2)),Math.toDegrees(Math.atan(this.imag / this.real)) );
+				case "radian":
+					return new Polar( Math.sqrt(Math.pow(this.real,2) + Math.pow(this.imag,2)),Math.atan(this.imag / this.real) );
+				default:
+					return new Polar(0,0);
+			}
+		} catch (ArithmeticException ae) {
+					return new Polar(0,0);
+		}
+	}
+
+	public String toString() {
+        if(this.imag < 0) {
+            return String.valueOf(this.real + "-" + (-this.imag) + "i");
+        } else {
+            return String.valueOf(this.real + "+" +this.imag + "i");
+        }
+    }
+}
---
+++ Numerical/Polar.java
@@ -0,0 +1,85 @@
+/**
+ * Polar.java
+ *
+ * @author dogan can karatas
+ * @version 1.0
+ */
+
+package Numerical;
+
+public class Polar {
+    private double radius;
+    private double angle;
+    private String unit = "degree"; // "degree", "radian"
+
+    public Polar() {
+        this.radius = 0;
+        this.angle = 0;
+    }
+
+    public Polar(double R, double theta) {
+        this.radius = R;
+        this.angle = theta;
+    }
+
+    public void set(Polar p) {
+        this.radius = p.getRadius();
+        this.angle = p.getAngle();
+    }
+
+    public void set(double R, double theta) {
+        this.radius = R;
+        this.angle = theta;
+    }
+
+    public Polar get() {
+        return new Polar(this.radius,this.angle);
+    }
+
+    public void setRadius(double R) {
+        this.radius = R;
+    }
+
+    public void setAngle(double theta) {
+        this.angle = theta;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public double getAngle() {
+        return this.angle;
+    }
+
+    public boolean compareTo(Polar p) {
+        if(this.radius == p.getRadius() && this.angle == p.getAngle()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean compareTo(double R, double theta) {
+        if(this.radius == R && this.angle == theta) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Complex toComplex(){
+        switch(this.unit) {
+            case "degree":
+                return new Complex((this.radius * Math.cos(Math.toRadians(this.angle))), (this.radius * Math.sin(Math.toRadians(this.angle))));
+            case "radian":
+                return new Complex((this.radius * Math.cos(Math.toRadians(this.angle))), (this.radius * Math.sin(this.angle)));
+            default:
+                return new Complex(0, 0);
+        }
+    }
+
+    public String toString(){
+        return String.valueOf(this.radius + "|" + this.angle);
+    }
+}
