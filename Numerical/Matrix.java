79257212109df3c0a68a1e8822aefb7fd7b102a5
Doğan Can KARATAŞ <dogan.karatas@yandex.com>
2017-05-18 05:16:16 +0300

    Alpha Release

--- Numerical/Matrix.java
+++ Numerical/Matrix.java
@@ -24,17 +24,16 @@ package OpenCI.Numerical;
    
 public class Matrix {
     public enum Type{
-        /*
-        B1, B2, B3, B4,     // 8 bit (byte)
-        S1, S2, S3, S4,     // 16 bit (short)
-        I1, I2, I3, I4,     // 32 bit (int)
-        F1, F2, F3, F4,     // 32 bit (float)
-        L1, L2, L3, L4,     // 64 bit (long int) **/
+        Byte,       // 8 bit (byte)
+        Short,      // 16 bit (short)
+        Int,    // 32 bit (int)
+        Float,      // 32 bit (float)
+        Long,       // 64 bit (long int)
         Double      // 64 bit (double/long float)
     };
     private int m;
     private int n;
-    private double[][] data_d; // n = 1 (1D) double matrix
+    private double[][] data_d; // double matrix
     private float[][] data_f;
     private long[][] data_l;
     private short[][] data_s;
@@ -46,13 +45,28 @@ public class Matrix {
 
     }
     
-    public Matrix(int rows, int cols,Type t) {
+    public Matrix(int rows, int cols, Type t) {
+        this.m = rows;
+        this.n = cols;
+        this.type = t;
         switch(t){
             case Double: // 1 channel double matrix
-                this.m = rows;
-                this.n = cols;
-                this.type = Type.Double;
                 this.data_d = new double[m][n];
+                break;
+            case Float:
+                this.data_f = new float[m][n];
+                break;
+            case Long:
+                this.data_l = new long[m][n];
+                break;
+            case Int:
+                this.data_i = new int[m][n];
+                break;
+            case Short:
+                this.data_s = new short[m][n];
+                break;
+            case Byte:
+                this.data_b = new byte[m][n];
                 break;
             default:
                 break;
@@ -72,12 +86,29 @@ public class Matrix {
     // fix these 
     
     public void cloneTo(Matrix mat) {
+        mat.setRow(this.getRow());
+        mat.setCol(this.getCol());
+        mat.setType(this.getType());
         switch(this.type) {
             case Double:
-                mat.setRow(this.getRow());
-                mat.setCol(this.getCol());
-                mat.setType(this.getType());
                 mat.data_d = this.data_d;
+                break;
+            case Float:
+                mat.data_f = this.data_f;
+                break;
+            case Long:
+                mat.data_l = this.data_l;
+                break;
+            case Int:
+                mat.data_i = this.data_i;
+                break;
+            case Short:
+                mat.data_s = this.data_s;
+                break;
+            case Byte:
+                mat.data_b = this.data_b;
+                break;
+            default:
                 break;
         }
     }
@@ -98,10 +129,82 @@ public class Matrix {
                     }
                 }
                 break;
+            case Float:
+                System.out.print("[");
+                for(int i = 0; i < this.m; i++) {
+                    if(i == 0) System.out.print("["); else System.out.print(" [");
+                    for(int j = 0; j < this.n; j++) {
+                        System.out.printf(" %7.3f",this.data_f[i][j]);
+                        if (j == (this.n - 1) && i != (this.m - 1)) 
+                            System.out.println(" ]"); 
+                        else if(j == (this.n - 1) && i == (this.m - 1)) 
+                            System.out.println(" ]]");
+                        else continue;
+                    }
+                }
+                break;
+            case Long:
+                System.out.print("[");
+                for(int i = 0; i < this.m; i++) {
+                    if(i == 0) System.out.print("["); else System.out.print(" [");
+                    for(int j = 0; j < this.n; j++) {
+                        System.out.printf(" %7d",this.data_l[i][j]);
+                        if (j == (this.n - 1) && i != (this.m - 1)) 
+                            System.out.println(" ]"); 
+                        else if(j == (this.n - 1) && i == (this.m - 1)) 
+                            System.out.println(" ]]");
+                        else continue;
+                    }
+                }
+                break;
+            case Int:
+                System.out.print("[");
+                for(int i = 0; i < this.m; i++) {
+                    if(i == 0) System.out.print("["); else System.out.print(" [");
+                    for(int j = 0; j < this.n; j++) {
+                        System.out.printf(" %7d",this.data_i[i][j]);
+                        if (j == (this.n - 1) && i != (this.m - 1)) 
+                            System.out.println(" ]"); 
+                        else if(j == (this.n - 1) && i == (this.m - 1)) 
+                            System.out.println(" ]]");
+                        else continue;
+                    }
+                }
+                break;
+            case Short:
+                System.out.print("[");
+                for(int i = 0; i < this.m; i++) {
+                    if(i == 0) System.out.print("["); else System.out.print(" [");
+                    for(int j = 0; j < this.n; j++) {
+                        System.out.printf(" %7d",this.data_s[i][j]);
+                        if (j == (this.n - 1) && i != (this.m - 1)) 
+                            System.out.println(" ]"); 
+                        else if(j == (this.n - 1) && i == (this.m - 1)) 
+                            System.out.println(" ]]");
+                        else continue;
+                    }
+                }
+                break;
+            case Byte:
+                System.out.print("[");
+                for(int i = 0; i < this.m; i++) {
+                    if(i == 0) System.out.print("["); else System.out.print(" [");
+                    for(int j = 0; j < this.n; j++) {
+                        System.out.printf(" %7d",this.data_b[i][j]);
+                        if (j == (this.n - 1) && i != (this.m - 1)) 
+                            System.out.println(" ]"); 
+                        else if(j == (this.n - 1) && i == (this.m - 1)) 
+                            System.out.println(" ]]");
+                        else continue;
+                    }
+                }
+                break;
+            default: 
+                break;
         }
     }
     
-        public void println() {
+    public void println() {
         switch(this.type){
             case Double:
                 System.out.print("[");
@@ -116,6 +219,78 @@ public class Matrix {
                         else continue;
                     }
                 }
+                break;
+            case Float:
+                System.out.print("[");
+                for(int i = 0; i < this.m; i++) {
+                    if(i == 0) System.out.print("["); else System.out.print(" [");
+                    for(int j = 0; j < this.n; j++) {
+                        System.out.printf(" %7.3f",this.data_f[i][j]);
+                        if (j == (this.n - 1) && i != (this.m - 1)) 
+                            System.out.println(" ]"); 
+                        else if(j == (this.n - 1) && i == (this.m - 1)) 
+                            System.out.println(" ]]\n");
+                        else continue;
+                    }
+                }
+                break;
+            case Long:
+                System.out.print("[");
+                for(int i = 0; i < this.m; i++) {
+                    if(i == 0) System.out.print("["); else System.out.print(" [");
+                    for(int j = 0; j < this.n; j++) {
+                        System.out.printf(" %7d",this.data_l[i][j]);
+                        if (j == (this.n - 1) && i != (this.m - 1)) 
+                            System.out.println(" ]"); 
+                        else if(j == (this.n - 1) && i == (this.m - 1)) 
+                            System.out.println(" ]]\n");
+                        else continue;
+                    }
+                }
+                break;
+            case Int:
+                System.out.print("[");
+                for(int i = 0; i < this.m; i++) {
+                    if(i == 0) System.out.print("["); else System.out.print(" [");
+                    for(int j = 0; j < this.n; j++) {
+                        System.out.printf(" %7d",this.data_i[i][j]);
+                        if (j == (this.n - 1) && i != (this.m - 1)) 
+                            System.out.println(" ]"); 
+                        else if(j == (this.n - 1) && i == (this.m - 1)) 
+                            System.out.println(" ]]\n");
+                        else continue;
+                    }
+                }
+                break;
+            case Short:
+                System.out.print("[");
+                for(int i = 0; i < this.m; i++) {
+                    if(i == 0) System.out.print("["); else System.out.print(" [");
+                    for(int j = 0; j < this.n; j++) {
+                        System.out.printf(" %7d",this.data_s[i][j]);
+                        if (j == (this.n - 1) && i != (this.m - 1)) 
+                            System.out.println(" ]"); 
+                        else if(j == (this.n - 1) && i == (this.m - 1)) 
+                            System.out.println(" ]]\n");
+                        else continue;
+                    }
+                }
+                break;
+            case Byte:
+                System.out.print("[");
+                for(int i = 0; i < this.m; i++) {
+                    if(i == 0) System.out.print("["); else System.out.print(" [");
+                    for(int j = 0; j < this.n; j++) {
+                        System.out.printf(" %7d",this.data_b[i][j]);
+                        if (j == (this.n - 1) && i != (this.m - 1)) 
+                            System.out.println(" ]"); 
+                        else if(j == (this.n - 1) && i == (this.m - 1)) 
+                            System.out.println(" ]]\n");
+                        else continue;
+                    }
+                }
+                break;
+            default: 
                 break;
         }
     }
@@ -145,18 +320,43 @@ public class Matrix {
     }
     
     public void setCell(int row, int col, double data) { 
-        // nD, Double
-        switch(this.type) { 
-            case Double:
-                this.data_d[row][col] = data;
-                break;
-        }
+        this.data_d[row][col] = data;
+    }
+    
+    public void setCell(int row, int col, float data) { 
+        this.data_f[row][col] = data;
+    }
+    
+    public void setCell(int row, int col, long data) { 
+        this.data_l[row][col] = data;
+    }
+    
+    public void setCell(int row, int col, int data) { 
+        this.data_i[row][col] = data;
+    }
+    
+    public void setCell(int row, int col, short data) { 
+        this.data_s[row][col] = data;
+    }
+
+    public void setCell(int row, int col, byte data) { 
+        this.data_b[row][col] = data;
     }
     
     public double getCell(int row,int col) {
         switch(this.type) {
             case Double:
                 return this.data_d[row][col];
+            case Float:
+                return this.data_f[row][col];
+            case Long:
+                return this.data_l[row][col];
+            case Int:
+                return this.data_i[row][col];
+            case Short:
+                return this.data_s[row][col];
+            case Byte:
+                return this.data_b[row][col];
             default:
                 return 0;
         }
